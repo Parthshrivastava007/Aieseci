38749aac83ffcc87453eb007683c240836fbeecb
Parthshrivastava007 <r17gamingofficial@gmail.com>
2026-05-02 23:38:45 +0530

    Renamed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
     { name: "Home", path: "/" },
     { name: "About", path: "/about" },
     { name: "Courses", path: "/courses" },
-    { name: "Student-Dashboard", path: "/enrollementtable" },
+    { name: "Admissions", path: "/enrollementtable" },
     { name: "Ledger", path: "/feetracker" },
     { name: "Contact", path: "/contact" },
   ];
